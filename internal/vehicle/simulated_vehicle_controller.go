73b5f5dfa07328dbbaec76b4b5300d2c96ab2ae2
Deseteral <deseteral@gmail.com>
2025-04-13 17:18:05 +0200

    Implement simulated vehicle controller

--- internal/vehicle/simulated_vehicle_controller.go
+++ internal/vehicle/simulated_vehicle_controller.go
@@ -1,12 +1,15 @@
 package vehicle
 
-type SimulatedVehicleController struct{}
+type SimulatedVehicleController struct {
+	chargingAmps int
+}
 
 func (c *SimulatedVehicleController) GetChargingAmps(vehicle *Vehicle) (amps int, error error) {
-	return 16, nil
+	return c.chargingAmps, nil
 }
 
 func (c *SimulatedVehicleController) SetChargingAmps(vehicle *Vehicle, chargingAmps int) error {
+	c.chargingAmps = chargingAmps
 	return nil
 }
 
