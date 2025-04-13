1932305f0e39d24aa31fb3fbffe0e6602d1e5a7e
Deseteral <deseteral@gmail.com>
2025-04-13 15:37:54 +0200

    Embed static files in binary

--- internal/webapp/server.go
+++ internal/webapp/server.go
@@ -1,25 +1,37 @@
 package webapp
 
 import (
+	"embed"
 	"fmt"
 	"github.com/a-h/templ"
 	"github.com/deseteral/resistere/internal/configuration"
 	"github.com/deseteral/resistere/internal/webapp/view"
+	"io/fs"
 	"log"
 	"net/http"
 )
 
+//go:embed static
+var staticFiles embed.FS
+
 func StartWebServerBlocking(config *configuration.Config) error {
 	addr := fmt.Sprintf(":%v", config.Web.Port)
 
+	var staticFs = fs.FS(staticFiles)
+	htmlContent, err := fs.Sub(staticFs, "static")
+	if err != nil {
+		log.Printf("Error starting web server: %v.\n", err)
+		return err
+	}
+
 	http.Handle("/", templ.Handler(view.Index()))
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(htmlContent))))
 
 	log.Printf("Web server starting on port %v.\n", config.Web.Port)
 
-	err := http.ListenAndServe(addr, nil)
+	err = http.ListenAndServe(addr, nil)
 	if err != nil {
-		log.Println("Error starting web server:")
+		log.Printf("Error starting web server: %v.\n", err)
 		return err
 	}
 
