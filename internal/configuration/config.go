6c4297029d76a11e5426df4cf3f36afa38d236ca
Deseteral <deseteral@gmail.com>
2026-07-18 23:56:17 +0200

    Parse Wall Connector config

--- internal/configuration/config.go
+++ internal/configuration/config.go
@@ -8,12 +8,13 @@ import (
 )
 
 type Config struct {
-	SimulatorMode    bool `toml:"simulator_mode"`
-	Web              Web
-	Controller       Controller
-	SolarmanInverter SolarmanInverter `toml:"solarman_inverter"`
-	TeslaControl     TeslaControl     `toml:"tesla_control"`
-	Vehicles         Vehicles
+	SimulatorMode      bool `toml:"simulator_mode"`
+	Web                Web
+	Controller         Controller
+	SolarmanInverter   SolarmanInverter `toml:"solarman_inverter"`
+	TeslaControl       TeslaControl     `toml:"tesla_control"`
+	Vehicles           Vehicles
+	TeslaWallConnector TeslaWallConnector `toml:"tesla_wall_connector"`
 }
 
 type Web struct {
@@ -34,6 +35,10 @@ type SolarmanInverter struct {
 
 type TeslaControl struct {
 	KeyFile string `toml:"key_file"`
+}
+
+type TeslaWallConnector struct {
+	Ip string
 }
 
 type Vehicles struct {
